62eb4c38895cd374f269da0e1ff5f4a20a0f4173
Joseph Stalnaker <josephlstalnaker@gmail.com>
2019-10-05 03:06:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,17 @@
 def addition(num1, num2)
   num1 + num2
 end
-  addition(5,4)
+addition(5,4)
 
 def subtraction(num1, num2)
   num1 - num2
 end
-  subtraction(10,5)
+subtraction(10,5)
   
 def division(num1, num2)
 
 end
+division(50,2)
 
 def multiplication(num1, num2)
 
